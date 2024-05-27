8191c66de0cfe87ff073ebcd0289a894962f34b5
Reddy9100 <129190164+Reddy9100@users.noreply.github.com>
2024-05-27 10:56:36 +0530

    Update dbConfig.js

--- config/dbConfig.js
+++ config/dbConfig.js
@@ -3,7 +3,7 @@ const mysql2 = require("mysql2");
 const connection = mysql2.createPool({
 host :"localhost",
 password :"root123",
-database : "test_db2",
+database : "dockerdatabase",
 user : "root"
 });
 
