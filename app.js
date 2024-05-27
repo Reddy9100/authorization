bbff386485bcb5434ce18784198ee02d9ed69e12
Reddy9100 <129190164+Reddy9100@users.noreply.github.com>
2024-05-27 10:57:33 +0530

    Update app.js

--- app.js
+++ app.js
@@ -11,8 +11,8 @@ app.use(express.json())
 
 app.use(cors())
 
-app.listen(process.env.PORT,()=>{
-    console.log (`Server is running on port ${process.env.PORT}`)
+app.listen(3000,()=>{
+    console.log ("Server is running on port 3000")
 })
 
 app.get("/",(req,res)=>{
