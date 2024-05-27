423d9ebeeddb68549080fa48051d7b0423bfb624
Reddy9100 <129190164+Reddy9100@users.noreply.github.com>
2024-05-27 10:37:25 +0530

    Update dbConfig.js

--- config/dbConfig.js
+++ config/dbConfig.js
@@ -1,10 +1,10 @@
 const mysql2 = require("mysql2");
 
 const connection = mysql2.createPool({
-  host: process.env.localhost,
-  user: process.env.user,
-  database: process.env.database,
-  password: process.env.password,
+host = "localhost",
+password = "root123",
+database = "test_db2",
+user = "root"
 });
 
 module.exports = connection
